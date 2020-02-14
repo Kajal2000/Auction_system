b0daf825832c468dbce88defe5db5b0ceb63fd20
kajal <you@example.com>
2020-02-14 19:02:51 +0530

    task done

--- routes/auction.js
+++ routes/auction.js
@@ -19,8 +19,9 @@ app.get("/api/:auction_id",(req,res)=>{
     let auction_id = req.params.auction_id
     var data_1 = appDB.get_by_id(auction_id);
     data_1.then((data)=>{
-        let auction_id = data[0]["auction_id"]
-        if (data[0]["price"] < req.body.bidder_price){
+        for(var k = 0; k<data.length; k++ ){
+        let auction_id = data[k]["auction_id"]
+        if (data[k]["price"] < req.body.bidder_price){
             let store = {
                 "bidder_name" : req.body.bidder_name,
                 "bidder_price" : req.body.bidder_price,
@@ -33,6 +34,7 @@ app.get("/api/:auction_id",(req,res)=>{
             }).catch((err)=>{ 
                 console.log(err)
             })
+        }
         }
     })
 })
